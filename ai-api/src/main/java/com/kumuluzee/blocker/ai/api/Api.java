d89eaab796930d39d345b923dd500a3fab5509f5
sandi.rezonja@gmail.com <sandi.rezonja@gmail.com>
2019-10-23 12:58:24 +0200

    First ai commit

---
+++ ai-api/src/main/java/com/kumuluzee/blocker/ai/api/Api.java
@@ -0,0 +1,7 @@
+package com.kumuluzee.blocker.ai.api;
+
+public class Api {
+    public static void main(String args[]) {
+        System.out.println("Hello world4!" + Api2.onePlusOne());
+    }
+}
---
+++ ai-api/src/main/java/com/kumuluzee/blocker/ai/api/Api2.java
@@ -0,0 +1,7 @@
+package com.kumuluzee.blocker.ai.api;
+
+public class Api2 {
+    public static int onePlusOne(){
+        return 1+1;
+    }
+}
