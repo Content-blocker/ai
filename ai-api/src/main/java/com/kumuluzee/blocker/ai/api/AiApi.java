5f575881c9bb49fbb8ce73aac5cd11182d4d96e3
ssandir <sandi.rezonja@gmail.com>
2020-01-03 19:10:00 +0100

    custom discovery

--- ai-api/src/main/java/com/kumuluzee/blocker/ai/api/AiApi.java
+++ ai-api/src/main/java/com/kumuluzee/blocker/ai/api/AiApi.java
@@ -6,42 +6,47 @@ import com.kumuluz.ee.discovery.enums.AccessType;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
 @RequestScoped
 @Path("/api")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public class AiApi {
-    @Inject
-    @DiscoverService(value = "ai", environment = "test", version = "1.0.0", accessType = AccessType.GATEWAY)
-    Optional<String> aiUrlString;
+    static Optional<WebTarget> aiTarget = CustomDiscovery.discover("ai", "test", "1.0.0");
+    static Optional<WebTarget> providerTarget = CustomDiscovery.discover("provider", "test", "1.0.0");
+    static String aiString = aiTarget.get().getUri().toString();
+    static String providerString = providerTarget.get().getUri().toString();
 
-    @Inject
-    @DiscoverService(value = "provider", environment = "test", version = "1.0.0", accessType = AccessType.GATEWAY)
-    Optional<String> providerUrlString;
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getResources() {
+        String links = "";
+        if(aiTarget.isPresent()){
+            links += "<a href='"+ aiString + "/api/integrations'>ai/api/integrations</a><br>";
+        }
+        if(providerTarget.isPresent()){
+            links += "<a href='"+ providerString + "/api'>provider/api</a><br>";
+        }
+
+        return "Hellow world! <br> I am intelligent. <br>" + links;
+    }
 
     @GET
     @Path("/integrations")
-    public String integrations(){
-        return "ai: " + aiUrlString.toString() + "\n" +
-                "provider: " + providerUrlString.toString() + "\n";
-    }
-
-    @GET
-    @Path("/proxyprovider")
-    public String proxyprovider(){
-        if(providerUrlString.isPresent()){
-           return providerUrlString.get();
+    public String integrations() {
+        String out = "";
+        if (aiTarget.isPresent()) {
+            out += "ai: " + aiString + "\n";
         }
-        return "notin";
+        else out+= "missing ai \n";
+        if (providerTarget.isPresent()) {
+            out += "provider: " + providerString + "\n";
+        }
+        else out+= "missing provider \n";
+        return out;
     }
-
-    @GET
-    public String getResources() {
-        return "Hellow world! <br> I am intelligent.";
-    }
-
-
 }
